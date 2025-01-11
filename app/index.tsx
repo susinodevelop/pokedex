a0409be5573065741aeddf6fe10786798629611f
Jesus Fernandez <susinodevelop@gmail.com>
2025-01-11 13:13:48 +0100

    Pokemon infinite scroll with tanstack query

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "expo-router";
 import { FlatList, StyleSheet, View } from "react-native";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import getPokemons from "@/actions/get-pokemons";
 import PokeballBg from "@/components/ui/PokeballBg";
 import { Text } from "react-native-paper";
@@ -13,22 +13,27 @@ const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
   const router = useRouter();
 
-  const { isLoading, data } = useQuery({
-    queryKey: ["pokemons"],
-    queryFn: () => getPokemons(0, 20),
-    staleTime: 1000 * 60 * 60,
+  const { isLoading, data, fetchNextPage } = useInfiniteQuery({
+    queryKey: ["pokemons", "pokemons-infinite"],
+    initialPageParam: 0,
+    queryFn: (params) => getPokemons(params.pageParam),
+    getNextPageParam: (lastPage, allPages) => allPages.length,
+    staleTime: 1000 * 60 * 60, // 1 hora
   });
 
   return (
     <View style={globalTheme.globalMargin}>
       <PokeballBg style={styles.imgPosition} />
       <FlatList
-        data={data}
+        data={data?.pages.flat() ?? []}
         keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
         numColumns={2}
         style={{ marginTop: top + 20 }}
         ListHeaderComponent={() => <Text variant="displayMedium">Pokedex</Text>}
         renderItem={({ item }) => <PokemonCard pokemon={item} />}
+        onEndReachedThreshold={0.6}
+        onEndReached={() => fetchNextPage()}
+        showsVerticalScrollIndicator={true}
       />
     </View>
   );
